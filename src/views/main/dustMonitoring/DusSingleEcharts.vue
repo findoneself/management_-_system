a07885341210818f091a7627a1981235759e0234
xpei <1697505279@qq.com>
2022-04-06 12:34:56 +0800

    单设备bug

--- src/views/main/dustMonitoring/DusSingleEcharts.vue
+++ src/views/main/dustMonitoring/DusSingleEcharts.vue
@@ -21,7 +21,7 @@
           v-model="dataForm.area"
           clearable
           placeholder="请选择"
-          @change="areaChange"
+          @change="getJczdList"
         >
           <el-option
             v-for="item in dictOptions.areaList"
@@ -36,7 +36,7 @@
           v-model="dataForm.monitoringSourceId"
           clearable
           placeholder="请选择"
-          @change="pickerChange('jkzd')"
+          @change="getDataList"
         >
           <el-option
             v-for="item in dictOptions.jczdList"
@@ -57,10 +57,11 @@
           unlink-panels
           clearable
           range-separator="至"
+          value-format="yyyy-MM-dd"
           start-placeholder="开始日期"
           end-placeholder="结束日期"
           :picker-options="pickerOptions"
-          @change="pickerChange"
+          @change="getDataList"
         >
         </el-date-picker>
       </el-form-item>
@@ -68,7 +69,10 @@
         label="类型："
         label-width="5rem"
       >
-        <el-select v-model="dataForm.type" @change="typeChange">
+        <el-select
+          v-model="dataForm.type"
+          @change="typeChange"
+        >
           <el-option
             v-for="item in dictOptions.typeList"
             :key="item.id"
@@ -81,7 +85,10 @@
         label="参数类型："
         style="width: 100%;"
       >
-        <el-checkbox-group v-model="dataForm.paramTypes" @change="pickerChange">
+        <el-checkbox-group
+          v-model="dataForm.paramTypes"
+          @change="getDataList"
+        >
           <el-checkbox
             :label="item.prop"
             v-for="item in dictOptions.paramTypesList"
@@ -103,9 +110,10 @@
     </el-form>
 
     <MultilineChart
-     v-if="tabsType !== 'table'"
-     :seriesData ='seriesDatas'
-     :xAxisData ='xAxisDatas'/>
+      v-if="tabsType !== 'table'"
+      :seriesData='seriesDatas'
+      :xAxisData='xAxisDatas'
+    />
 
   </TableForm>
 </template>
@@ -129,7 +137,7 @@ export default {
         // 监测站点
         jczdList: [],
         // 类型
-        typeList: [{id:'zt', name:'昨天数据'}, {id:'jqt', name:'近七天数据'}, {id:'dy', name:'当月数据'}, {id:'dn', name:'当年数据'}],
+        typeList: [{ id: 'zt', name: '昨天数据' }, { id: 'jqt', name: '近七天数据' }, { id: 'dy', name: '当月数据' }, { id: 'dn', name: '当年数据' }],
         // 参数类型
         paramTypesList: [],
         // 数据图表
@@ -141,7 +149,7 @@ export default {
       // 查询表单
       dataForm: {
         area: 'xz-1',
-        date: '',
+        date: [],
         paramTypes: ['wd'],
         monitoringSourceId: '',
         type: 'zt'
@@ -150,13 +158,7 @@ export default {
       dataLoading: false,
       // 表格数据---echart原始数据
       dataList: [{ jcd: 21, sj: 15, wd: 24343, qy: 3534 }],
-      columns: [
-        { name: '湿度', prop: 'jcd', key: 3 },
-        { name: '时间', prop: 'sj', tooltip: true, key: 1 },
-        { name: '温度', prop: 'wd', key: 2 },
-        { name: '气压', prop: 'qy', key: 4 }
-      ],
-      info:{},
+      columns: [],
       // echarts的参数类型
       echartsType: '',
       // 时间快捷选项
@@ -191,183 +193,163 @@ export default {
       tabsType: '',
       // ehcart----数据
       // x轴
-      xAxisDatas:['3.01', '3.02', '3.03', '3.04', '3.05', '3.06', '3.07', '3.08', '3.09', '3.10', '3.11', '3.12'],
+      xAxisDatas: [],
       // 数据
       seriesDatas: [],
-      api:{},
-      dusindex:false
-    }
-  },
-  created () {
-    this.initDict()
-    this.dictOptions.areaList = JSON.parse(sessionStorage.getItem('areaList'))||[]
-    this.dataForm.area = this.dictOptions.areaList[0].id ||''
-     let router = this.$route.path.slice(16)
-    if(router==='DusIndex'){
-      this.dusindex = true
-      this.api = {
-        jczdListApi:'/dustMonitoringSource/list/', // 监测站点
-        paramTypesApi:'/dustMonitoringSource/paramList', // 参数类型
-        dataListApi:'/dustMonitoringSource/deviceData/one' // 列表
-      }
-
-    this.getJczdList()
-    this.getParamsType()
-    this.getDataList()
-    }else{
-      this.api = {}
-    }
-    this.tabsClick(this.dictOptions.tabsTypes[0].id)
-    this.dataForm.date = this.$format.getTwodaysDate()
-  },
-  methods: {
-    initDict () {
-      const dict = this.$store.state.global.dictData
-      console.log(dict)
-      if (dict.paramsType && dict.paramsType.length > 0) {
-        this.dictOptions.paramTypesList = dict.paramsType
-        this.dataForm.paramTypes = [dict.paramsType[0].id]
-      }
-      if (dict.xzarea && dict.xzarea.length > 0) {
-        this.dictOptions.areaList = dict.xzarea
-        this.dataForm.area = dict.xzarea[0].id
-      }
-    },
-    // 点击回调-当前组件只有导出
-    buttonClick () {
-      if (this.tabsType === 'table') {
-        // 表格按钮点击回调--导出表格
-
-      }
-    },
-    // 设置展示类型
-    tabsClick (id) {
-      this.tabsType = id
-      if (id === 'echart') {
-        // this.echartsType = this.tformHead.btnList[0].id
-      }
-    },
-    // 获取表格和统计数据
-    getDataList () {
-      this.dataLoading = true
-      let {date} = this.dataForm
-      this.$http({
-        url: this.api.dataListApi,
-        method: 'post',
-        data:{
-          'dateStart': date[0],
-          'dateEnd': date[1],
-           ...this.dataForm
-        }
-      }).then(res => {
-        this.dataLoading = false
-        const {data, status} = res
-        console.log(data)
-        if (status === 200) {
-          const {columns, seriesdata, xaxisdata} = data.data.chart
-          let arr = []
-          columns.map(i => {
-            arr.push({...i, key:i.KEY})
-          })
-          this.columns = arr
-          this.dataList = data.data.table || []
-          console.log(this.seriesDatas)
-          this.seriesDatas.forEach((i, index) => {
-            if (index>=0){
-              delete this.seriesDatas[index]
-            }
-          })
-          this.seriesDatas = []
-          console.log(this.seriesDatas)
-          this.seriesDatas = seriesdata
-          this.xAxisDatas = xaxisdata
-          console.log(this.seriesDatas)
-        } else {
-          this.$message.error('获取统计数据失败！')
-          this.dataList = []
-        }
-      }, () => {
-        this.dataLoading = false
-        this.$message.error('获取统计数据失败！')
-      })
-    },
-    // 行政区域变化
-    areaChange (){
-      this.getJczdList()
-      this.getDataList()
-    },
-    pickerChange (value){
-      if(value === 'jkzd'){
-      this.getInfo()
-      }
-      this.getDataList()
-    },
-    // 检测站点
-    getJczdList (){
-      this.$http({
-        url: this.api.jczdListApi+this.dataForm.area,
-         method:'post'
-      }).then(res => {
-        console.log(res)
-        const { data, status} = res
-        if(status===200){
-          this.dictOptions.jczdList = data.data || []
-          this.dataForm.monitoringSourceId = this.dictOptions.jczdList[0].id|| ''
-          sessionStorage.setItem('jczdList', JSON.stringify(data.data))
-          this.getInfo()
-          }else{
-          this.$message.error('获取行政数据错误')
-        }
-      })
-    },
-    // 参数
-    getParamsType (){
-      this.$http({
-        url: this.api.paramTypesApi
-      }).then(res => {
-        console.log(res)
-        const { data, status} = res
-        if(status===200){
-          this.dictOptions.paramTypesList = data.data || []
-          this.dataForm.paramTypes = [this.dictOptions.paramTypesList[0].prop] || []
-          sessionStorage.setItem('paramTypesList', JSON.stringify(data.data))
-          this.getDataList()
-          }else{
-          this.$message.error('获取行政数据错误')
-        }
-      })
-    },
-    typeChange (value){
-      console.log(value)
-      let res
-      if(value==='zt'){
-        res = this.$format.getTwodaysDate()
-      }else if(value==='jqt'){
-        res = this.$format.getDefaultWorkDatetime()
-      }else if(value==='dy'){
-        res = this.$format.getMonthStartDatetime()
-      }else{
-        res=this.$format.getYearStartDatetime()
-      }
-      console.log(res)
-      this.dataForm.date = res
-      this.getDataList()
-    },
-    getInfo (){
-      let item = this.dictOptions.jczdList.find(i => i.id===this.dataForm.monitoringSourceId)
-      this.info = { title: item.name, small: '2022-03-01 ( 小时数据 )' }
+      api: {},
+      dusindex: false
     }
   },
   computed: {
     tformHead () {
       // 需要取参数类型和选择日期的信息
-      const info = {...this.info}
+      let item = this.dictOptions.jczdList.find(i => i.id === this.dataForm.monitoringSourceId) || { name: '' }
+      let info = {
+        title: item.name,
+        small: (this.dataForm.date.length > 0 ? `${this.dataForm.date[0]}-${this.dataForm.date[1]}` : '') + '（分钟数据）'
+      }
       if (this.tabsType === 'table') {
         info.btnType = 'elbtn'
         info.btnList = [{ id: 'export', name: '导出Excel', type: 'primary', size: 'medium' }]
       }
-       console.log(info)
       return info
+    }
+  },
+  created () {
+    this.dictOptions.areaList = JSON.parse(sessionStorage.getItem('areaList')) || []
+    this.dataForm.area = this.dictOptions.areaList[0].id || ''
+    // 判断当前路由
+    let router = this.$route.path.slice(16)
+    if (router === 'DusIndex') {
+      this.dusindex = true
+      this.api = {
+        jczdListApi: '/dustMonitoringSource/list/', // 监测站点
+        paramTypesApi: '/dustMonitoringSource/paramList', // 参数类型
+        dataListApi: '/dustMonitoringSource/deviceData/one' // 列表
+      }
+    } else {
+      this.api = {}
+    }
+    this.tabsClick(this.dictOptions.tabsTypes[0].id)
+    this.dataForm.date = this.$format.getTwodaysDate()
+    this.getParamsType()
+  },
+  methods: {
+    // 点击回调-当前组件只有导出
+    buttonClick () {
+      if (this.tabsType === 'table') {
+        // 表格按钮点击回调--导出表格
+      }
+    },
+    // 设置展示类型
+    tabsClick (id) {
+      this.tabsType = id
+    },
+    // 获取表格和统计数据
+    getDataList () {
+      this.dataLoading = true
+      const params = this._cloneDeep(this.dataForm)
+      if (params.date.length > 0) {
+        params.dateStart = params.date[0]
+        params.dateEnd = params.date[1]
+      } else {
+        params.dateStart = ''
+        params.dateEnd = ''
+      }
+      delete params.date
+      this.$http({
+        url: this.api.dataListApi,
+        method: 'post',
+        data: params
+      }).then(res => {
+        this.dataLoading = false
+        const { data, code, msg } = res.data
+        if (code === 200) {
+          this.dataList = data.table || []
+          // 图表数据
+          if (data.chart) {
+            this.columns = data.chart.columns.map(i => {
+              i.key = i.KEY
+              return i
+            })
+            this.seriesDatas = data.chart.seriesdata || []
+            this.xAxisDatas = data.chart.xaxisdata || []
+          }
+        } else {
+          this.$message.error(msg || '获取统计数据失败！')
+          this.dataList = []
+          this.seriesDatas = []
+          this.xAxisDatas = []
+        }
+      }, (err) => {
+        this.dataLoading = false
+        this.$message.error(err.data.msg || '获取统计数据失败！')
+        this.dataList = []
+        this.seriesDatas = []
+        this.xAxisDatas = []
+      })
+    },
+    // 事件类型改变
+    typeChange (value) {
+      let res
+      if (value === 'zt') {
+        res = this.$format.getTwodaysDate()
+      } else if (value === 'jqt') {
+        res = this.$format.getDefaultWorkDatetime()
+      } else if (value === 'dy') {
+        res = this.$format.getMonthStartDatetime()
+      } else {
+        res = this.$format.getYearStartDatetime()
+      }
+      this.dataForm.date = res
+      this.getDataList()
+    },
+    // 检测站点
+    getJczdList () {
+      this.$http({
+        url: this.api.jczdListApi + this.dataForm.area,
+        method: 'post'
+      }).then(res => {
+        const { data, code, msg } = res.data
+        if (code === 200) {
+          this.dictOptions.jczdList = data || []
+          if (this.dictOptions.jczdList.length > 0) {
+            this.dataForm.monitoringSourceId = data[0].id || ''
+            this.getDataList()
+          }
+        } else {
+          this.dictOptions.jczdList = []
+          this.dataForm.monitoringSourceId = ''
+          this.$message.error(msg || '获取检测站点数据失败！')
+        }
+      }, (err) => {
+        this.$message.error(err.data.msg || '获取检测站点数据失败！')
+        this.dictOptions.jczdList = []
+        this.dataForm.monitoringSourceId = ''
+      })
+    },
+    // 参数
+    getParamsType () {
+      this.$http({
+        url: this.api.paramTypesApi
+      }).then(res => {
+        const { data, code, msg } = res.data
+        if (code === 200) {
+          this.dictOptions.paramTypesList = data || []
+          if (this.dictOptions.paramTypesList.length > 0) {
+            this.dataForm.paramTypes = [data[0].prop] || []
+          }
+          this.getJczdList()
+        } else {
+          this.$message.error(msg || '获取行政数据错误')
+          this.dictOptions.paramTypesList = []
+          this.dataForm.paramTypes = []
+        }
+      }, (err) => {
+        this.$message.error(err.data.msg || '获取行政数据错误')
+        this.dictOptions.paramTypesList = []
+        this.dataForm.paramTypes = []
+      })
     }
   }
 }
--- src/views/main/dustMonitoring/components/MultilineChart.vue
+++ src/views/main/dustMonitoring/components/MultilineChart.vue
@@ -11,16 +11,13 @@ export default {
     xAxisData: {
       type: Array,
       default () {
-        return ['3.01', '3.02', '3.03', '3.04', '3.05', '3.06', '3.07', '3.08', '3.09', '3.10', '3.11', '3.12']
+        return []
       }
     },
     seriesData: {
       type: Array,
       default () {
-        return [{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], color: '#19B17E', title: '云-徐州传染病医院' },
-        { data: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], color: '#9834FF', title: '云-淮安传染病医院' },
-        { data: [13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35], color: '#23DBFC', title: '云-延安传染病医院' },
-        { data: [16, 18, 20, 22, 24, 26, 28, 30, 31, 33, 35, 37], color: '#FA6B16', title: '云-常州传染病医院' }]
+        return []
       }
     },
     legendData: {
@@ -32,8 +29,8 @@ export default {
   },
   data () {
     return {
-      myChart:null,
-      color:['#19B17E', '#9834FF', '#23DBFC', '#FA6B16'],
+      myChart: null,
+      color: ['#19B17E', '#9834FF', '#23DBFC', '#FA6B16'],
       option: {
         xAxis: {
           type: 'category',
@@ -109,31 +106,28 @@ export default {
       }
     }
   },
-    watch:{
-    seriesData:{
+  watch: {
+    seriesData: {
       handler () {
         this.getmap()
       },
-      deep:true
+      deep: true
     }
   },
   mounted () {
     this.getmap()
   },
-    activated () {
-    // 由于给echart添加了resize事件, 在组件激活时需要重新resize绘画一次, 否则出现空白bug
-    if (this.myChart) {
-      this.myChart.resize()
-    }
-  },
   methods: {
     getmap () {
+      if (!this.myChart) {
+        this.myChart = this.$echarts.init(document.getElementById('many_line_chart'))
+      }
+      this.myChart.clear()
       this.option.legend.data = this.legendData
       this.option.xAxis.data = this.xAxisData
+      // 处理series数据
       let series = []
-      if(this.seriesData.length>0){
-        console.log(123)
-       this.seriesData.map((i, index) => {
+      this.seriesData.map((i, index) => {
         series.push({
           name: i.title,
           data: i.data,
@@ -163,49 +157,10 @@ export default {
           }
         })
       })
-      }else{
-        console.log('没有data')
-        let arr = [{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], color: '#19B17E', title: '云-徐州传染病医院' },
-        { data: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], color: '#9834FF', title: '云-淮安传染病医院' },
-        { data: [13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35], color: '#23DBFC', title: '云-延安传染病医院' },
-        { data: [16, 18, 20, 22, 24, 26, 28, 30, 31, 33, 35, 37], color: '#FA6B16', title: '云-常州传染病医院' }]
-       arr.map((i, index) => {
-        series.push({
-          name: i.title,
-          data: i.data,
-          type: 'line',
-          //  折线图线上的原点
-          symbol: 'circle',
-          symbolSize: 15,
-          itemStyle: {
-            normal: {
-              color: this.color[index],
-              borderColor: '#fff',
-              fontSize: '1rem',
-              shadowBlur: 0,
-              label: {
-                show: false
-              },
-              labelLine: {
-                show: false
-              }
-            }
-          },
-          lineStyle: {
-            normal: {
-              color: i.color,
-              width: 5
-            }
-          }
-        })
-      })
-      }
-     let option = {
+      let option = {
         ...this.option,
         series
       }
-      console.log(option)
-      this.myChart = this.$echarts.init(document.getElementById('many_line_chart'))
       this.myChart.setOption(option)
       window.addEventListener('resize', function () {
         this.myChart.resize()
