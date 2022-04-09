f776749cbcb3ba7957129744ab13925edc370171
xpei <1697505279@qq.com>
2022-04-09 13:19:19 +0800

    合并

--- src/network/api.js
+++ src/network/api.js
@@ -1,6 +1,12 @@
 import $http from './index'
 let apiObj = {}
-// 直接调取文件请求文件流
+/** *
+ * 根据用户的参数请求系统的文件流并且下载文件
+ * @param url
+ * @param params
+ * @param filename
+ * @param callback
+ */
 apiObj.downloadBlob = function (url, params, filename = '文件', callback) {
   $http({
     url: url,
@@ -11,13 +17,19 @@ apiObj.downloadBlob = function (url, params, filename = '文件', callback) {
     apiObj.download(response, filename, callback)
   })
 }
-// 根据文件流直接下载文件
+/** *
+ * 系统根据请求的Response对象下载文件
+ * @param response
+ * @param fileName
+ * @param callback
+ */
 apiObj.download = function (response, fileName = '文件', callback) {
   try {
     if (!response || response.status !== 200) {
-      callback({ msg: response.msg || '访问服务器网络错误，请检查！' })
+      callback.call(this, { msg: response.msg || '访问服务器网络错误，请检查！' })
       return
     }
+    callback.call(this, true)
     let url = window.URL.createObjectURL(response.data)
     let link = document.createElement('a')
     link.style.display = 'none'
@@ -26,7 +38,7 @@ apiObj.download = function (response, fileName = '文件', callback) {
     document.body.appendChild(link)
     link.click()
   } catch (e) {
-    callback(false)
+    callback.call(this, false)
   }
 }
 export default apiObj
